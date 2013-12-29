5542b0e6e277373527102184900f9a2b0bac8205
Pierre Dubois <duboisp2@gmail.com>
2013-12-29 01:26:09 -0500

    re-formating

--- working-examples/v4-migration/simpleCustomized-en/flot-json.js
+++ working-examples/v4-migration/simpleCustomized-en/flot-json.js
@@ -1,6 +1,50 @@
 
-[{"data":[[-0.5,1995068],[0.5,2885113],[1.5,3794772]],"label":"Patent Searches","color":"#191970","bars":{"show":true,"barWidth":0.15,"align":"center"}},{"data":[[-0.33333333333333337,2767011],[0.6666666666666666,2798982],[1.6666666666666667,2889591]],"label":"Trademark Searches","color":"#483D8B","bars":{"show":true,"barWidth":0.15,"align":"center"}},{"data":[[-0.16666666666666669,1072753],[0.8333333333333333,1151086],[1.8333333333333333,915725]],"label":"Home Page Access","color":"#A9A9A9","bars":{"show":true,"barWidth":0.15,"align":"center"}},{"data":[[0,191424],[1,223449],[2,229687]],"label":"Copyright Searches","color":"#696969","bars":{"show":true,"barWidth":0.15,"align":"center"}},{"data":[[0.16666666666666663,18815],[1.1666666666666665,26609],[2.1666666666666665,24820]],"label":"Commissioner's Decision Searches","color":"#C0C0C0","bars":{"show":true,"barWidth":0.15,"align":"center"}},{"data":[[0.33333333333333326,76331],[1.3333333333333333,78339],[2.333333333333333,66283]],"label":"Industrial Design","color":"#6B8E23","bars":{"show":true,"barWidth":0.15,"align":"center"}}]
-{"canvas":true,"xaxis":{"ticks":[[0,"2008-09"],[1,"2009-10"],[2,"2010-11"]]},"yaxis":{"max":4000000}}
+[
+	{
+		"data":[[-0.5,1995068],[0.5,2885113],[1.5,3794772]],
+		"label":"Patent Searches",
+		"color":"#191970",
+		"bars":{"show":true,"barWidth":0.15,"align":"center"}
+	},
+	{
+		"data":[[-0.33333333333333337,2767011],[0.6666666666666666,2798982],[1.6666666666666667,2889591]],
+		"label":"Trademark Searches",
+		"color":"#483D8B",
+		"bars":{"show":true,"barWidth":0.15,"align":"center"}
+	},
+	{
+		"data":[[-0.16666666666666669,1072753],[0.8333333333333333,1151086],[1.8333333333333333,915725]],
+		"label":"Home Page Access",
+		"color":"#A9A9A9",
+		"bars":{"show":true,"barWidth":0.15,"align":"center"}
+	},
+	{
+		"data":[[0,191424],[1,223449],[2,229687]],
+		"label":"Copyright Searches",
+		"color":"#696969",
+		"bars":{"show":true,"barWidth":0.15,"align":"center"}
+	},
+	{
+		"data":[[0.16666666666666663,18815],[1.1666666666666665,26609],[2.1666666666666665,24820]],
+		"label":"Commissioner's Decision Searches",
+		"color":"#C0C0C0",
+		"bars":{"show":true,"barWidth":0.15,"align":"center"}
+	},
+	{
+		"data":[[0.33333333333333326,76331],[1.3333333333333333,78339],[2.333333333333333,66283]],
+		"label":"Industrial Design",
+		"color":"#6B8E23",
+		"bars":{"show":true,"barWidth":0.15,"align":"center"}
+	}
+]
+
+{
+	"canvas":true,
+	"xaxis": {"ticks":[[0,"2008-09"],[1,"2009-10"],[2,"2010-11"]]},
+	"yaxis":{"max":4000000}
+}
+
+
 
 
 [{"data":[[0,72189],[1,72297],[2,70387]],"label":"Enquiries","color":"#191970","bars":{"show":true,"barWidth":0.9,"align":"center"}}]
