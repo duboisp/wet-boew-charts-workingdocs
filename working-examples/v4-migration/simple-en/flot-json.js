c69592bb9552ba318e47e6ab62cee233072e4556
Pierre Dubois <pierre.dubois@ic.gc.ca>
2013-12-29 01:09:16 -0500

    simple-en - Complete Json structure

---
+++ working-examples/v4-migration/simple-en/flot-json.js
@@ -0,0 +1,35 @@
+
+[{"data":[[0,50],[1,100],[2,150]],"label":"Row heading 1","color":"#8d201c","lines":{"show":true,"fill":true}},{"data":[[0,210],[1,25],[2,75]],"label":"Row heading 2","color":"#EE8310","lines":{"show":true,"fill":true}},{"data":[[0,1000],[1,245],[2,964]],"label":"Row heading 3","color":"#2a7da6","lines":{"show":true,"fill":true}}]
+{"canvas":true,"xaxis":{"ticks":[[0,"col1"],[1,"col2"],[2,"col3"]]}}
+
+
+[{"data":[[0,300],[1,160],[2,40],[3,120],[4,100],[5,70]],"label":"Mary","color":"#8d201c"},{"data":[[0,3],[1,40],[2,30],[3,45],[4,35],[5,49]],"label":"Tom","color":"#EE8310"},{"data":[[0,10],[1,180],[2,10],[3,85],[4,70],[5,79]],"label":"Brad","color":"#2a7da6"},{"data":[[0,40],[1,80],[2,90],[3,25],[4,15],[5,119]],"label":"Kate","color":"#5a306b"}]
+{"canvas":true,"xaxis":{"ticks":[[0,"nourriture"],[1,"auto"],[2,"domestiques"],[3,"meubles"],[4,"cuisine"],[5,"bain"]]}}
+
+
+[{"data":[[0,50],[1,100],[2,150]],"label":"En-tête de rangée 1","color":"#8d201c","lines":{"show":true,"fill":true}},{"data":[[0,210],[1,25],[2,75]],"label":"En-tête de rangée 2","color":"#EE8310","lines":{"show":true,"fill":true}},{"data":[[0,1000],[1,245],[2,964]],"label":"En-tête de rangée 3","color":"#2a7da6","lines":{"show":true,"fill":true}}]
+{"canvas":true,"xaxis":{"ticks":[[0,"col1"],[1,"col2"],[2,"col3"]]}}
+
+
+[{"data":[[-0.5,300],[0.5,160],[1.5,40],[2.5,120],[3.5,100],[4.5,70]],"label":"Mary","color":"#8d201c","bars":{"show":true,"barWidth":0.225,"align":"center"}},{"data":[[-0.25,3],[0.75,40],[1.75,30],[2.75,45],[3.75,35],[4.75,49]],"label":"Tom","color":"#EE8310","bars":{"show":true,"barWidth":0.225,"align":"center"}},{"data":[[0,10],[1,180],[2,10],[3,85],[4,70],[5,79]],"label":"Brad","color":"#2a7da6","bars":{"show":true,"barWidth":0.225,"align":"center"}},{"data":[[0.25,40],[1.25,80],[2.25,90],[3.25,25],[4.25,15],[5.25,119]],"label":"Kate","color":"#5a306b","bars":{"show":true,"barWidth":0.225,"align":"center"}}]
+{"canvas":true,"xaxis":{"ticks":[[0,"nourriture"],[1,"auto"],[2,"domestiques"],[3,"meubles"],[4,"cuisine"],[5,"bain"]]}}
+
+
+[{"label":"nourriture","color":"#8d201c","data":[[0,40]]},{"label":"auto","color":"#EE8310","data":[[0,80]]},{"label":"domestiques","color":"#2a7da6","data":[[0,90]]},{"label":"meubles","color":"#5a306b","data":[[0,25]]},{"label":"cuisine","color":"#285228","data":[[0,15]]},{"label":"bain","color":"#154055","data":[[0,119]]}]
+{"series":{"pie":{"show":true}}}
+
+
+[{"label":"nourriture","color":"#8d201c","data":[[0,10]]},{"label":"auto","color":"#EE8310","data":[[0,180]]},{"label":"domestiques","color":"#2a7da6","data":[[0,10]]},{"label":"meubles","color":"#5a306b","data":[[0,85]]},{"label":"cuisine","color":"#285228","data":[[0,70]]},{"label":"bain","color":"#154055","data":[[0,79]]}]
+{"series":{"pie":{"show":true}}}
+
+
+[{"label":"nourriture","color":"#8d201c","data":[[0,3]]},{"label":"auto","color":"#EE8310","data":[[0,40]]},{"label":"domestiques","color":"#2a7da6","data":[[0,30]]},{"label":"meubles","color":"#5a306b","data":[[0,45]]},{"label":"cuisine","color":"#285228","data":[[0,35]]},{"label":"bain","color":"#154055","data":[[0,49]]}]
+{"series":{"pie":{"show":true}}}
+
+
+[{"label":"nourriture","color":"#8d201c","data":[[0,300]]},{"label":"auto","color":"#EE8310","data":[[0,160]]},{"label":"domestiques","color":"#2a7da6","data":[[0,40]]},{"label":"meubles","color":"#5a306b","data":[[0,120]]},{"label":"cuisine","color":"#285228","data":[[0,100]]},{"label":"bain","color":"#154055","data":[[0,70]]}]
+{"series":{"pie":{"show":true}}}
+
+
+[{"label":"Value","color":"#8d201c","data":[[0,300]]}]
+{"series":{"pie":{"show":true}}}
